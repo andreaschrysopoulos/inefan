fc07870dd51fbe84a79f221f11fc5521dc9094ec
Andreas Chrysopoulos <andreaschrysopoulos@gmail.com>
2025-04-16 23:42:23 +0300

    image caching?

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,20 @@
 import { withPayload } from '@payloadcms/next/withPayload'
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/:all*(jpg|jpeg|png|svg|webp|gif)",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=31536000, immutable"
+          }
+        ]
+      }
+    ]
+  }
+}
 
 export default withPayload(nextConfig)
